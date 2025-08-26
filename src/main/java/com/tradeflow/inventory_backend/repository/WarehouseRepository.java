6aa3e35c4d5b47fd30136050a689d18c92a2eded
masudbappy <masud.bappy90@gmail.com>
2025-08-26 11:30:04 +0600

    Updated CategoryRepository, SupplierRepository, TypeRepository and WarehouseRepository

--- src/main/java/com/tradeflow/inventory_backend/repository/WarehouseRepository.java
+++ src/main/java/com/tradeflow/inventory_backend/repository/WarehouseRepository.java
@@ -4,6 +4,9 @@ import com.tradeflow.inventory_backend.model.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+	Optional<Warehouse> findByWarehouseNameAndLocation(String warehouseName, String location);
 }
